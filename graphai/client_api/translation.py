240e5b08dd73d6d100f3a30e06d64374f83acbe1
Yves Renier <yves.renier@epfl.ch>
2024-02-08 23:05:03 +0100

    split large list of translations

--- graphai/client_api/translation.py
+++ graphai/client_api/translation.py
@@ -7,7 +7,7 @@ from graphai.client_api.utils import get_response, task_result_is_ok, split_text
 
 def translate_text(
         text: Union[str, list], source_language, target_language, graph_ai_server='http://127.0.0.1:28800',
-        sections=('GRAPHAI', 'TRANSLATE'), force=False, debug=False, max_text_length=None
+        sections=('GRAPHAI', 'TRANSLATE'), force=False, debug=False, max_text_length=None, max_text_list_length=50000
 ):
     if source_language == target_language:
         return text
@@ -25,8 +25,8 @@ def translate_text(
     # use english as an intermediary if needed
     if source_language not in ('en', 'fr') and target_language != 'en':
         translated_text_en = translate_text(
-            text, source_language, 'en', graph_ai_server=graph_ai_server,
-            sections=sections, force=force, debug=debug, max_text_length=max_text_length
+            text, source_language, 'en', graph_ai_server=graph_ai_server, sections=sections, force=force, debug=debug,
+            max_text_length=max_text_length, max_text_list_length=max_text_list_length
         )
         if translated_text_en is None:
             status_msg(
@@ -35,9 +35,57 @@ def translate_text(
             )
             return None
         return translate_text(
-            translated_text_en, 'en', target_language, graph_ai_server=graph_ai_server,
-            sections=sections, force=force, debug=debug, max_text_length=max_text_length
+            translated_text_en, 'en', target_language, graph_ai_server=graph_ai_server, sections=sections, force=force,
+            debug=debug, max_text_length=max_text_length, max_text_list_length=max_text_list_length
         )
+    # split in smaller lists if the text is too large
+    if isinstance(text, list):
+        lengths_text = [len(t) for t in text]
+        if sum(lengths_text) > max_text_list_length:
+            idx_start = 0
+            sum_length = 0
+            n_text_elems = len(text)
+            translated_text_full = [None]*n_text_elems
+            for idx_end in range(n_text_elems):
+                sum_length += lengths_text[idx_end]
+                # we reached the end
+                if idx_end + 1 == n_text_elems:
+                    status_msg(
+                        f'get part of the text (from {idx_start} to the {n_text_elems}) as the full list is too long',
+                        color='grey', sections=list(sections) + ['PROCESSING']
+                    )
+                    translated_text_full[idx_start:] = translate_text(
+                        text[idx_start:], source_language, target_language, graph_ai_server=graph_ai_server,
+                        sections=sections, force=force, debug=debug, max_text_length=max_text_length,
+                        max_text_list_length=max_text_list_length
+                    )
+                # one element is already too large
+                elif sum_length > max_text_list_length:
+                    status_msg(
+                        f'get part of the text (at {idx_start}) as the full list is too long',
+                        color='grey', sections=list(sections) + ['PROCESSING']
+                    )
+                    translated_text_full[idx_start] = translate_text(
+                        text[idx_start], source_language, target_language, graph_ai_server=graph_ai_server,
+                        sections=sections, force=force, debug=debug, max_text_length=max_text_length,
+                        max_text_list_length=max_text_list_length
+                    )
+                    idx_start += 1
+                    sum_length = 0
+                # with the next element it is too large or we reached the end
+                elif sum_length + lengths_text[idx_end + 1] > max_text_list_length:
+                    status_msg(
+                        f'get part of the text (from {idx_start} to {idx_end}) as the full list is too long',
+                        color='grey', sections=list(sections) + ['PROCESSING']
+                    )
+                    translated_text_full[idx_start:idx_end+1] = translate_text(
+                        text[idx_start:idx_end+1], source_language, target_language, graph_ai_server=graph_ai_server,
+                        sections=sections, force=force, debug=debug, max_text_length=max_text_length,
+                        max_text_list_length=max_text_list_length
+                    )
+                    idx_start = idx_end + 1
+                    sum_length = 0
+            return translated_text_full
     # get rid of None in list input
     if isinstance(text, list):
         text_to_translate = []
@@ -76,7 +124,7 @@ def translate_text(
     task_id = response_translate.json()['task_id']
     # wait for the translation to be completed
     tries_translate_status = 0
-    while tries_translate_status < 3000:
+    while tries_translate_status < 3600:
         tries_translate_status += 1
         try:
             response_translate_status = get_response(
@@ -117,13 +165,15 @@ def translate_text(
                 )
                 if max_text_length:
                     return translate_text(
-                        text, source_language, target_language, graph_ai_server=graph_ai_server,
-                        sections=sections, force=force, debug=debug, max_text_length=max_text_length-200
+                        text, source_language, target_language, graph_ai_server=graph_ai_server, sections=sections,
+                        force=force, debug=debug, max_text_length=max_text_length-200,
+                        max_text_list_length=max_text_list_length
                     )
                 else:
                     return translate_text(
                         text, source_language, target_language, graph_ai_server=graph_ai_server,
-                        sections=sections, force=force, debug=debug, max_text_length=2000
+                        sections=sections, force=force, debug=debug, max_text_length=2000,
+                        max_text_list_length=max_text_list_length
                     )
             else:
                 status_msg(
