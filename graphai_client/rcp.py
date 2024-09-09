e8d3d3f71a06486a8a58b83b15ca64451287724f
Yves Renier <yves.renier@epfl.ch>
2024-09-09 12:56:09 +0200

    add compute_embeddings_of_concepts_on_rcp()

--- graphai_client/rcp.py
+++ graphai_client/rcp.py
@@ -3,6 +3,7 @@ from re import fullmatch
 from requests import Session
 from isodate import parse_duration
 from email.utils import parsedate_to_datetime
+from json import dumps
 from typing import List, Tuple, Optional
 from graphai_client.utils import (
     status_msg, get_video_link_and_size, get_http_header, strfdelta, convert_subtitle_into_segments,
@@ -1000,6 +1001,58 @@ def detect_concept_from_persons_on_rcp(
                         'MixedScore'
                     )
                 )
+
+
+def compute_embeddings_of_concepts_on_rcp(
+        page_ids: List[int], graph_api_json=None, login_info=None, piper_mysql_json_file=None, batch_size=1000,
+        temp_tables=True
+):
+    if login_info is None or 'token' not in login_info:
+        login_info = login(graph_api_json)
+    num_pages = len(page_ids)
+    if num_pages > batch_size:
+        for idx in range(0, num_pages, batch_size):
+            compute_embeddings_of_concepts_on_rcp(
+                page_ids, graph_api_json=graph_api_json, login_info=login_info,
+                piper_mysql_json_file=piper_mysql_json_file, batch_size=batch_size
+            )
+        return
+    schema = 'gen_wikipedia'
+    page_neighbours_table = 'Pages_Neighbours'
+    page_content_table = 'Page_Content_Full'
+    if temp_tables:
+        page_neighbours_table += '_tmp'
+        page_content_table += '_tmp'
+    with get_piper_connection(piper_mysql_json_file) as piper_connection:
+        concepts_info = execute_query(
+            piper_connection,
+            f"""SELECT 
+                p.PageID, 
+                pc.OpeningText
+            FROM {schema}.{page_neighbours_table} as p
+            INNER JOIN {schema}.{page_content_table} AS pc USING (PageID)
+            WHERE p.PageID IN ({', '.join([str(p_id) for p_id in page_ids])});"""
+        )
+        text_to_embed = []
+        page_id_of_embedded_text = []
+        for page_id, opening_text in concepts_info:
+            page_id_of_embedded_text.append(page_id)
+            text_to_embed.append(opening_text)
+        embedded_texts = embed_text(text_to_embed, login_info=login_info, max_text_length=400)
+        assert len(embedded_texts) == len(page_id_of_embedded_text)
+        data_embeddings = [
+            (dumps([round(e, 8) for e in embedding]), page_id)
+            for page_id, embedding in zip(page_id_of_embedded_text, embedded_texts)
+        ]
+        update_data_into_table(
+            piper_connection, schema=schema, table_name=page_neighbours_table,
+            columns=['Embedding'], pk_columns=['PageID'], data=data_embeddings
+        )
+        piper_connection.commit()
+    status_msg(
+        f'Embeddings have been computed for {num_pages} concepts in the ontology neighbourhood', color='green',
+        sections=['GRAPHAI', 'EMBED CONCEPT', 'SUCCESS']
+    )
 
 
 def fix_subtitles_translation_on_rcp(
